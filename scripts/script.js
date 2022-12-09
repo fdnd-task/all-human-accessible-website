0caea1e74a95aa2a5204320b54187f0dcf7ba0f0
Sascha-davidson <saschadavidson1@gmail.com>
2022-12-09 15:25:45 +0100

    responsive header reparatie + js uitvouw menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,36 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-items');
-  const navlinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//  const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.mobile-nav');
+//   const navlinks = document.querySelectorAll('.nav-links li');
 
+//   burger.addEventListener('click', () => {
+//     //toggle nav\
 
-  burger.addEventListener('click', () => {
-    //toggle nav\
+//     nav.classList.toggle('nav-active');
 
-    nav.classList.toggle('nav-active');
+//     //animete links
+//     navlinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = '';
+//       } else {
+//         link.style.animation = `navlinkfade 0.5s ease forwards ${(navlinks.length+index-1-(index*2))/7 + 0.25}s`;
+//       }
+//     });
+//     //burger animation
+//     burger.classList.toggle('toggle');
 
-    //animete links
-    navlinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navlinkfade 0.5s ease forwards ${(navlinks.length+index-1-(index*2))/7 + 0.25}s`;
-      }
-    });
-    //burger animation
-    burger.classList.toggle('toggle');
+//   });
 
-  });
+// }
 
+// $("pre").html(function (index, html) {
+//   return html.replace(/^(.*)$/mg, "<span class=\"line\">$1</span>")
+// });//
 
+let burger = document.querySelector("#burger");
+let nav = document.querySelector("#mobile-nav");
 
-}
-
-$("pre").html(function (index, html) {
-  return html.replace(/^(.*)$/mg, "<span class=\"line\">$1</span>")
-});
+burger.addEventListener('click', () => {
+  console.log(nav.classList)
+  nav.classList.toggle('open-menu')
+})
