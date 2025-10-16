2be087d673f22c2cca8601d7e9e15b125e6d5f75
Chewsy <13772434+Chewsy@users.noreply.github.com>
2025-10-16 12:57:13 +0200

    Closed #3

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#back").addEventListener("click", () => {
-    history.back();
-  });
+    const articles = document.querySelectorAll('#articles article');
+
+    document.getElementById('hr-select').addEventListener('change', e => {
+        const selected = Array.from(e.target.selectedOptions, opt => opt.value).filter(v => v);
+
+        articles.forEach(article => {
+            const tags = Array.from(article.querySelectorAll('.tags dt'), dt => dt.textContent);
+            article.style.display = !selected.length || selected.some(v => tags.includes(v)) ? '' : 'none';
+        });
+    });
 });
