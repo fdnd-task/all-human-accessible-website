3c568a431e398a43387bf71715ab4a8dddc21b2e
Jamie Vos <Jamievos100@gmail.com>
2025-11-02 16:21:38 +0100

    New design

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,19 +1,49 @@
-const goTopBtn = document.getElementById("naarBovenKnop");
-const hamburger = document.getElementById('hamburger');
-const mobileMenu = document.getElementById('mobile-menu');
-const body = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+  const goTopBtn = document.getElementById("naarBovenKnop");
+  const hamburger = document.getElementById('hamburger');
+  const closeButton = document.getElementById('close');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const body = document.body;
 
-window.onscroll = function() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    goTopBtn.style.display = "block";
-  } else {
-    goTopBtn.style.display = "none";
-  }
-};
+  window.onscroll = function() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      goTopBtn.style.display = "block";
+    } else {
+      goTopBtn.style.display = "none";
+    }
+  };
 
-hamburger.addEventListener('click', () => {
-  const menuIsHidden = mobileMenu.classList.toggle('hidden');
+  document.querySelectorAll("footer li").forEach((li) => {
+    li.addEventListener("click", () => {
+      const content = li.nextElementSibling;
+  
+      const isOpen = li.classList.contains("open");
+  
+      document.querySelectorAll("footer li").forEach((item) => item.classList.remove("open"));
+      document.querySelectorAll("footer ul > div").forEach((div) => div.classList.remove("open"));
+  
+      if (!isOpen && content && content.tagName === "DIV") {
+        li.classList.add("open");
+        content.classList.add("open");
+      }
+    });
+  });
+  
+  
+  
+  
 
-  body.classList.toggle('no-scroll', !menuIsHidden);
+  hamburger.addEventListener('click', () => {
+    mobileMenu.classList.remove('hidden');
+    body.classList.add('no-scroll');
+  });
+
+  closeButton.addEventListener('click', () => {
+    mobileMenu.classList.add('hidden');
+    body.classList.remove('no-scroll');
+  });
+
+  goTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
 });
-
